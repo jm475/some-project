453ac65e89df0fa503e5a211b551cc4bb387bdce
John <johnmacdonald789@gmail.com>
2024-08-15 17:45:33 +1200

    style: Finished making LoginPage design

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css'
 import LoginPage from './LoginPage.tsx'
 import SignupPage from './SignupPage.tsx'
 
+// Main Entry point for the app
 function App() {
   // State to switch between Login or Signup pages
   const [pageState, setPageState] = useState('login')
 
   return (
     <div id="main">
-      {pageState === 'login' ? (<LoginPage />) : (<SignupPage />)}
+      {pageState === 'login' ? (<LoginPage setPageState={setPageState} />) : (<SignupPage />)}
       <button onClick={() => setPageState('login')}>Go to Login</button>
       <button onClick={() => setPageState('signup')}>Go to Signup</button>
     </div>
--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import './LoginPage.css'
-const LoginPage = ({}) => {
+
+
+
+// Define an interface for the props
+interface LoginPageProps {
+  // setPageState is a function that accepts a string ('login' or 'signup') and returns nothing
+  setPageState: (page: 'login' | 'signup') => void;
+}
+
+
+
+/**
+ * 
+ * LoginPage Component to allow users to login into the website
+ */
+const LoginPage: React.FC<LoginPageProps> = ({setPageState}) => {
 
     // States to handle email and password input
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
   
-    // const handleSubmit = (event) => {
-    //   event.preventDefault();
-    //   // Add logic to handle login (e.g., call an API, validate input)
-    //   //onLogin(email, password);
-    // };
-
     return (
         <div id="login-card">
           <h2>Login</h2>
@@ -36,7 +45,9 @@ const LoginPage = ({}) => {
                 required
               />
             </div>
-            <button type="submit">Login</button>
+            <a id="forgot-password" href="">Forgot Password?</a>
+            <button id="login-button" type="submit">Login</button>
+            <p>Dont have an account? <a href="#" onClick={() => setPageState('signup')}>Signup</a></p>
           </form>
         </div>
       );
